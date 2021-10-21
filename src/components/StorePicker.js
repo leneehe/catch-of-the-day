0fe4650ffeb5f810ce150d265cf5513ea556807e
Lena He <Lena@Lenas-iMac.local>
2021-10-21 05:38:51 -0400

    create the app and its components

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -5,7 +5,7 @@ class StorePicker extends React.Component {
     render() {
         return (
             <form className="store-selector">
-            {/* Comment here */}
+                {/* Comment here */}
                 <h2>Please Enter a Store</h2>
                 <input type="text" required placeholder="Store Name" />
                 <button type="submit">Visit Store →</button>
